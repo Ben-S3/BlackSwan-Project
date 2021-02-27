cc294b2ee1ca1a1b27675b8fc39ed18c2611f639
BradFranklin17 <78823978+BradFranklin17@users.noreply.github.com>
2021-02-27 15:45:50 -0600

    Updated for Inputs
    Messy inputs to get simple user interaction. 
    Needs: Multiple keywords, Optimized search when looking far back, and Multiple users tweets.

--- main.py
+++ main.py
@@ -10,14 +10,31 @@ auth.set_access_token('3930122292-KS4T2uHvgVkbiZ5utFrzYpfrfwsMJ85lJVHi6oT',
 
 api = tweepy.API(auth)
 
-page = 1
+user = input("What User to Search:: ")
+#Insert 1(one) word as a keyword search
+keyword = input("What Keyword to Search:: ")
+#Insert Year, Month, and Day as Integers
+start_year = input("What Start Year to Search:: ")
+start_month = input("What Start Month to Search:: ")
+start_day = input("What Start Day to Search:: ")
+end_year = input("What End Year to Search:: ")
+end_month = input("What End Month to Search:: ")
+end_day = input("What End Day to Search:: ")
+
+start_date = datetime.datetime(int(start_year), int(start_month), int(start_day), 0, 0, 0)
+end_date = datetime.datetime(int(end_year), int(end_month), int(end_day), 0, 0, 0)
+
+page = 1  # One Page includes 20 tweets.
 while True:
-    statuses = api.user_timeline("@TheOnion", page = page)
-    if statuses:
-        for status in statuses:
-            # process status here
-            print(status.text)
+    statuses = api.user_timeline(user, page = page)  # Select user and their timeline on a certain "page".
+    if statuses:  # As long as there are tweets on this page...
+        for status in statuses:  # Loop through the tweets...
+            if(status.created_at < start_date):
+                exit(0)  # Exit program if search date is over
+            if(keyword in status.text and status.created_at < end_date and status.created_at > start_date):
+                print(status.text)  # And print their text
+                print(status.created_at)
     else:
         # All done
         break
-    page += 1  # next page
+    page += 1  # Next page

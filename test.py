2cdf42d61dc9c400ac3f79a8eaf727ac71cf747e
Ben-S3 <50253113+Ben-S3@users.noreply.github.com>
2021-03-10 15:09:15 -0600

    Added geo_tag input to add geotag data to the search

--- test.py
+++ test.py
@@ -22,11 +22,14 @@ end_year = input("What End Year to Search:: ")
 end_month = input("What End Month to Search:: ")
 end_day = input("What End Day to Search:: ")
 
+geo_tag = input("Input coordinates and radius x,y,r :: ")
+
 start_date = datetime.datetime(int(start_year), int(start_month), int(start_day), 0, 0, 0)
 end_date = datetime.datetime(int(end_year), int(end_month), int(end_day), 0, 0, 0)
 keyword = keyword.split()
 
 for n in keyword:
-    for status1 in tweepy.Cursor(api.search, since=start_date, until=end_date, q=n).items(30):
+    for status1 in tweepy.Cursor(api.search, since=start_date, until=end_date, q=n, geo=geo_tag).items(30):
         print(status1.text)
         print(status1.geo)
+

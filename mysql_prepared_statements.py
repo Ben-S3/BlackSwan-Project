e77aaf1e8672efbd514991683dfc95cb48541dd0
hopechurch1 <78617973+hopechurch1@users.noreply.github.com>
2021-03-18 18:14:57 +0000

    Add files via upload

---
+++ mysql_prepared_statements.py
@@ -0,0 +1,13 @@
+# Author: Hope Church
+# Date Created: 3/17/2021
+# Date updated: 3/18/2021
+# Description: scratchpad of potentially useful sql statements
+
+#inserts
+"INSERT INTO user (username,website,displayname,follower_count,follow_count) VALUES ( %s,%s, %s,%s, %s)"
+
+#simple selects
+
+#join selects
+
+#updates

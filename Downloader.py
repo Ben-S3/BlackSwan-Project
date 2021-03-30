26e287d7598e988c48d86c81e8039fc7700a9497
Zoft29 <61292405+Zoft29@users.noreply.github.com>
2021-03-30 18:55:07 -0500

    Add files via upload

---
+++ Downloader.py
@@ -0,0 +1,52 @@
+#Author: Brian Contreras
+#Date: 3/30/2021
+#Description: A class to download the media data from a tweet
+
+import wget
+import tweepy
+from tweepy import OAuthHandler
+
+
+auth = OAuthHandler('P3MouxIBM8paKK5WU9nq8rkDQ', '8WFkiLm136rtGfiHc8LQNpiUaQvXHrXDybUFJ55SeijbAvBzjV')
+auth.set_access_token('3930122292-nE61z1YrkLLtfWCiDDLQJI6AylW62EJHBpZ8jWt',
+'ehRrZd1gvNCgHgrm3jr7wNP0hbn6tJxE8VusJxLH9Iybz')
+
+api = tweepy.API(auth)
+
+
+def donwloadMedia(tweetID):
+    tweet = getTweetData(tweetID)
+    if tweetHasMedia(tweet) == False:
+        print("Tweet does not have media")
+        return 0
+    else:
+        if mediaType(tweet) == "video":
+            downloadMediaFile(tweet)
+        elif mediaType(tweet) == "animated_gif":
+            downloadMediaFile(tweet)
+        elif mediaType(tweet) == "photo":
+            for tweet.extended_entities.media.media_url in tweet.extended_entities.media:
+                wget.download(tweet.extended_entities.media.media_url)
+
+
+def getTweetData(tweetID):
+    tweet = api.get_status(tweetID)
+    return tweet
+
+def tweetHasMedia(tweet):
+    if len(tweet.entities.get('media', []) > 0):
+        return True
+    else:
+        return False
+
+def mediaType(tweet):
+    if tweet.extended_entities.media.type == "photo":
+        return "photo"
+    elif tweet.extended_entities.media.type == "video":
+        return "video"
+    else:
+        return "animated_gif"
+
+def downloadMediaFile(tweet):
+    media_file = tweet.extended_entities.media.media_url
+    wget.download(media_file)

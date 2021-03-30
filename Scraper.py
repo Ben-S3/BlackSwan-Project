e5638731321ad550fba310a9dd0778acfdd28b4f
BradFranklin17 <78823978+BradFranklin17@users.noreply.github.com>
2021-03-30 15:45:05 -0500

    Update Scraper.py
    New and Improved realistic search, with Event Tags

--- Scraper.py
+++ Scraper.py
@@ -19,28 +19,32 @@ auth.set_access_token('3930122292-nE61z1YrkLLtfWCiDDLQJI6AylW62EJHBpZ8jWt',
 
 api = tweepy.API(auth)
 
-keyword = "Monster AND Hunter"
+keyword = "Suez Canal Evergreen Ever AND Given"
 start_year = 2021
 start_month = 3
-start_day = 23
+start_day = 26
 end_year = 2021
 end_month = 4
 end_day = 2
 
+tags = ["Suez", "Canal", "Evergreen", "Ever", "Given"]
+
 start_date = datetime.datetime(int(start_year), int(start_month), int(start_day), 0, 0, 0)
 end_date = datetime.datetime(int(end_year), int(end_month), int(end_day), 0, 0, 0)
 
-keyword += " -filter:retweets" #Ensure no retweets included.
-for status1 in tweepy.Cursor(api.search, since=start_date, until=end_date, q=keyword, tweet_mode = "extended").items(100): #For 100 tweets matching the search...
+Twitter_parser.parseSearchData("SuezCanalBlocked", start_date.date, start_date.time, end_date.date, end_date.time, 30.57, 32.29, "Suez Canal", 10, tags)
+
+keyword += " -filter:retweets"
+for status1 in tweepy.Cursor(api.search, since=start_date, until=end_date, q=keyword, tweet_mode = "extended").items(100):
     isComment = False
-    if status1.in_reply_to_status_id_str is not None: #Check to see if its a reply
+    if status1.in_reply_to_status_id_str is not None:
         isComment = True
     tweet = [None, status1.user.screen_name, "twitter.com", status1.user.name, None, None, None, None, 24, None,
              None, None, None, str(status1.created_at)[:10], str(status1.created_at)[11:], status1.full_text,
              status1.favorite_count, status1.retweet_count, 0, isComment, None,
              "https://twitter.com/twitter/status/" + status1.id_str, False, status1.lang,
              0, None, None, None, None, None, None]
-             #Create array of parser data
     print(tweet)
-    Twitter_parser.parseTweet(tweet) #Send Data array to the Parser
+    Twitter_parser.parseTweet(tweet)
+
 

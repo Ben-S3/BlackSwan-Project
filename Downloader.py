199363e168354634f8c337644e194e1be36fae01
Ben-S3 <50253113+Ben-S3@users.noreply.github.com>
2021-03-31 19:20:44 -0500

    corrections after testing

--- Downloader.py
+++ Downloader.py
@@ -1,4 +1,4 @@
-#Author: Brian Contreras
+#Author: Brian Contreras & BS
 #Date: 3/30/2021
 #Description: A class to download the media data from a tweet
 
@@ -14,7 +14,7 @@ auth.set_access_token('3930122292-nE61z1YrkLLtfWCiDDLQJI6AylW62EJHBpZ8jWt',
 api = tweepy.API(auth)
 
 
-def donwloadMedia(tweetID):
+def downloadMedia(tweetID):
     tweet = getTweetData(tweetID)
     if tweetHasMedia(tweet) == False:
         print("Tweet does not have media")
@@ -25,24 +25,24 @@ def donwloadMedia(tweetID):
         elif mediaType(tweet) == "animated_gif":
             downloadMediaFile(tweet)
         elif mediaType(tweet) == "photo":
-            for tweet.extended_entities.media.media_url in tweet.extended_entities.media:
-                wget.download(tweet.extended_entities.media.media_url)
+            for x in range(len(tweet.extended_entities['media'])):
+                wget.download(tweet.extended_entities['media'][x]['media_url'])
 
 
 def getTweetData(tweetID):
-    tweet = api.get_status(tweetID)
+    tweet = api.get_status(tweetID, tweet_mode="extended")
     return tweet
 
 def tweetHasMedia(tweet):
-    if len(tweet.entities.get('media', []) > 0):
+    if len(tweet.entities.get('media', [])) > 0:
         return True
     else:
         return False
 
 def mediaType(tweet):
-    if tweet.extended_entities.media.type == "photo":
+    if tweet.extended_entities['media'][0]['type'] == "photo":
         return "photo"
-    elif tweet.extended_entities.media.type == "video":
+    elif tweet.extended_entities['media'][0]['type'] == "video":
         return "video"
     else:
         return "animated_gif"
@@ -50,3 +50,4 @@ def mediaType(tweet):
 def downloadMediaFile(tweet):
     media_file = tweet.extended_entities.media.media_url
     wget.download(media_file)
+

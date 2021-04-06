410988a07bf6fc484e5f84d404240eb73c0b5aa4
BradFranklin17 <78823978+BradFranklin17@users.noreply.github.com>
2021-04-06 15:05:54 -0500

    Update Scraper for Geo
    Still needs specific testing on tweets with Geo Location attached directly to the tweet.

--- Scraper.py
+++ Scraper.py
@@ -1,6 +1,6 @@
-# Author: Bradley Franklin
+# Author: Bradley Franklin & Hope Church
 # Date Created: 2/9/2021
-# Date updated: 3/30/2021
+# Date updated: 4/6/2021
 # Description: Scraper of Twitter for Hard-Coded Demo of Useability
 
 import sys
@@ -9,6 +9,8 @@ import csv
 import datetime
 import tweepy
 from tweepy import OAuthHandler
+import geopy
+from geopy.geocoders import Nominatim
 
 import Twitter_parser
 
@@ -18,6 +20,8 @@ auth.set_access_token('3930122292-nE61z1YrkLLtfWCiDDLQJI6AylW62EJHBpZ8jWt',
                       'ehRrZd1gvNCgHgrm3jr7wNP0hbn6tJxE8VusJxLH9Iybz')
 
 api = tweepy.API(auth)
+
+geolocator = Nominatim(user_agent = "BSTwitterScraper")
 
 keyword = "Suez Canal Evergreen"
 start_year = 2021
@@ -38,21 +42,32 @@ end_time=end_date.strftime("%H:%M:%S")
 start_date=start_date.strftime("%Y-%m-%d")
 end_date=end_date.strftime("%Y-%m-%d")
 
-
-idevent=Twitter_parser.parseSearchData("SuezCanalBlocked", start_date, start_time, end_date, end_time, 30.57, 32.29, "Suez Canal", 10, tags)[1]
+idevent = Twitter_parser.parseSearchData("SuezCanalBlocked", start_date, start_time, end_date, end_time, 30.57, 32.29, "Suez Canal", 100, tags)[1]
 
 keyword += " -filter:retweets"
-geocode = "30.57,32.29,1000mi"
-for status1 in tweepy.Cursor(api.search, since=start_date, until=end_date, q=keyword, geocode = geocode, tweet_mode = "extended").items(100):
+geocode = "30.57,32.29,100mi"
+for status in tweepy.Cursor(api.search, since=start_date, until=end_date, q=keyword, geocode = geocode, tweet_mode = "extended").items(100):
     isComment = False
-    if status1.in_reply_to_status_id_str is not None:
+    if status.in_reply_to_status_id_str is not None:
         isComment = True
-    tweet = [None, status1.user.screen_name, "twitter.com", status1.user.name, None, None, None, None, 24, None,
-             None, None, None, str(status1.created_at)[:10], str(status1.created_at)[11:], status1.full_text,
-             status1.favorite_count, status1.retweet_count, 0, isComment, None,
-             "https://twitter.com/twitter/status/" + status1.id_str, False, status1.lang,
+    long = None
+    lat = None
+    loc = None
+    if status.geo is not None:
+        long = status.coordinates['coordinates'][0]
+        lat = status.coordinates['coordinates'][1]
+        location = geolocator.reverse(lat, long)
+        loc = location.address
+    elif status.user.location is not None:
+        loc = status.user.location
+        location = geolocator.geocode(status.user.location)
+        long = location.longitude
+        lat = location.latitude
+    tweet = [None, status.user.screen_name, "twitter.com", status.user.name, None, long, lat, loc, 10, None,
+             None, None, None, str(status.created_at)[:10], str(status.created_at)[11:], status.full_text,
+             status.favorite_count, status.retweet_count, 0, isComment, None,
+             "https://twitter.com/twitter/status/" + status.id_str, False, status.lang,
              0, None, None, None, None, None, None]
     print(tweet)
     Twitter_parser.parseTweet(tweet,idevent)
-
 

da40e44a2d9ca25b4721686fc4e2513585dda5ee
Zoft29 <61292405+Zoft29@users.noreply.github.com>
2021-04-13 16:05:33 -0500

    Update Downloader.py

--- Downloader.py
+++ Downloader.py
@@ -1,7 +1,7 @@
 #Author: Brian Contreras & BS & Bradley Franklin
 #Date: 3/30/2021
-#Update: 3/31/2021
-#Description: A class to download the media data from a tweet
+#Update: 4/13/2021
+#Description: A file to download the media data from a tweet
 
 import wget
 import urllib.request
@@ -15,24 +15,31 @@ auth.set_access_token('3930122292-nE61z1YrkLLtfWCiDDLQJI6AylW62EJHBpZ8jWt',
 
 api = tweepy.API(auth)
 
-
+#Function to download tweet data if available, then turns data into an array of BLOBS and returns the BLOB array
 def downloadMedia(tweetID):
+    #Stores tweet
     tweet = getTweetData(tweetID)
+    #Checks if the tweet has media files in it
     if tweetHasMedia(tweet) == False:
         print("Tweet does not have media")
-        return []
+        return 0
 
+    #If the tweet has media files
     else:
         blobs = []
+
+        #If the media type if a video
         if mediaType(tweet) == "video":
             filename = wget.download(tweet.extended_entities['media'][0]['media_url'])
             urllib.request.urlretrieve(tweet.extended_entities['media'][0]['media_url'], filename)
             blobs.append(turnToBLOB(filename))
             return blobs
 
+        #If the media type is a gif
         elif mediaType(tweet) == "animated_gif":
             wget.download(tweet.extended_entities['media'][0]['media_url'])
 
+        #If the media type is a photo
         elif mediaType(tweet) == "photo":
             for x in range(len(tweet.extended_entities['media'])):
                 filename = wget.download(tweet.extended_entities['media'][x]['media_url'])
@@ -40,16 +47,19 @@ def downloadMedia(tweetID):
             return blobs
 
 
+#Function to obtain the tweet itself from the tweet data
 def getTweetData(tweetID):
     tweet = api.get_status(tweetID, tweet_mode="extended")
     return tweet
 
+#Function to check if the tweet has media files in it
 def tweetHasMedia(tweet):
     if len(tweet.entities.get('media', [])) > 0:
         return True
     else:
         return False
 
+#Function to check the type of media the tweet has
 def mediaType(tweet):
     if tweet.extended_entities['media'][0]['type'] == "photo":
         return "photo"
@@ -58,6 +68,7 @@ def mediaType(tweet):
     else:
         return "animated_gif"
 
+#Function to turn media data into a BLOB object
 def turnToBLOB(filename):
     with open(filename, 'rb') as file:
         blob = file.read()

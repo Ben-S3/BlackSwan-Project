24438868101d744bcb961791c38361f416e13195
hopechurch1 <78617973+hopechurch1@users.noreply.github.com>
2021-04-22 16:45:07 -0500

    update demo code

--- demo.sh
+++ demo.sh
@@ -1,4 +1,4 @@
 sudo mysql blackswan_event_tracker < blackswan_create_schema.sql
-python3 Scraper.py
+python3 -m cProfile -s tottime Scraper.py > profile.txt
 sudo mysql blackswan_event_tracker < demo.sql >out.csv
-libreoffice out.csv
+#libreoffice out.csv

b3ce170d2f6b85773f18b07719333e61164ea344
bhavishah01 <51960840+bhavishah01@users.noreply.github.com>
2021-05-05 14:20:09 +0530

    Update Search.py

--- Search.py
+++ Search.py
@@ -1,45 +1,42 @@
 import sys
-import mysql.connector
-# from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QPushButton, QAction, QLineEdit, QMessageBox, QLabel, \
-#     QCalendarWidget, QComboBox
-# from PyQt5.QtGui import QIcon, QDoubleValidator, QIntValidator
-# from PyQt5.QtCore import pyqtSlot
-# import PyQt5
+from PyQt5.QtWidgets import QApplication, QWidget, QLineEdit, QTableView, QHeaderView, QVBoxLayout
+from PyQt5.QtCore import Qt, QSortFilterProxyModel
+from PyQt5.QtGui import QStandardItemModel, QStandardItem
 
+class AppDemo(QWidget):
+    def _init_(self):
+        super()._init_()
+        self.resize(1200, 1000)
+        mainLayout = QVBoxLayout()
+#for testing 
+        companies = ('Apple', 'Facebook', 'Google', 'Amazon', 'Walmart', 'Dropbox', 'Starbucks', 'eBay', 'Canon')
+        model = QStandardItemModel(len(companies), 1)
+        model.setHorizontalHeaderLabels(['Company'])
 
-usserinput = input("Enter the Event name ?")
-print(usserinput)
-# import Scraper
-cnx = mysql.connector.connect(host="localhost", port=8889, user="root", password="root", database="Blackswan")
-cursor = cnx.cursor()
-query = "SELECT idUser,username,website FROM Blackswan.Blackswan_table WHERE idUser = 4;"
+        for row, company in enumerate(companies):
+            item = QStandardItem(company)
+            model.setItem(row, 0, item)
 
+        filter_proxy_model = QSortFilterProxyModel()
+        filter_proxy_model.setSourceModel(model)
+        filter_proxy_model.setFilterCaseSensitivity(Qt.CaseInsensitive)
+        filter_proxy_model.setFilterKeyColumn(0)
 
+        search_field = QLineEdit()
+        search_field.setStyleSheet('font-size: 35px; height: 60px;')
+        search_field.textChanged.connect(filter_proxy_model.setFilterRegExp)
+        mainLayout.addWidget(search_field)
 
-cursor.execute(query)
-for i in cursor:
-    print(i)
-cursor = cnx.cursor()
+        table = QTableView()
+        table.setStyleSheet('font-size: 35px;')
+        table.verticalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        table.setModel(filter_proxy_model)
+        mainLayout.addWidget(table)
 
-class Example(QMainWindow):
+        self.setLayout(mainLayout)
 
-    def __init__(self):
-         super().__init__()
-
-        self.initUI()
-
-    def initUI(self):
-
-        self.setGeometry(300, 300, 250, 150)
-        self.setWindowTitle('Search Events')
-        self.show()
-
-
-def main():
-    app = QApplication(sys.argv)
-    ex = Example()
-    sys.exit(app.exec_())
-
-
-if __name__ == '__main__':
-     main()
+app = QApplication(sys.argv)
+demo = AppDemo()
+demo.show()
+sys.exit(app.exec_())

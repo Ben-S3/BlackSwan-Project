ad20eff6e5ee613c4443745d0388d896292f330f
BradFranklin17 <78823978+BradFranklin17@users.noreply.github.com>
2021-05-12 14:01:27 -0500

    Update DatabaseUI.py

--- DatabaseUI.py
+++ DatabaseUI.py
@@ -6,7 +6,7 @@
 import sys
 from PyQt5 import QtCore
 from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QPushButton, QAction, QLineEdit, QMessageBox, QLabel, QCalendarWidget, QComboBox
-from PyQt5.QtGui import QIcon, QDoubleValidator, QIntValidator
+from PyQt5.QtGui import QIcon, QDoubleValidator, QIntValidator,  QListWidget, QListWidgetItem, QVBoxLayout
 from PyQt5.QtCore import pyqtSlot
 import PyQt5
 import inserter

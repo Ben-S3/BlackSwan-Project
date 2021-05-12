7f9b52f0527cbee1baa50897dab401f5fd0a9c5a
BradFranklin17 <78823978+BradFranklin17@users.noreply.github.com>
2021-05-12 13:57:12 -0500

    Update DatabaseUI.py

--- DatabaseUI.py
+++ DatabaseUI.py
@@ -76,6 +76,36 @@ class App(QMainWindow):
         for x in self.events:
             self.eventLookup.addItem(x.name)
             self.eventNums[x.name] = x.id_
+            
+class PostDisplay(QMainWindow):
+
+    def __init__(self, parent=None):
+        super(App, self).__init__(parent)
+        self.title = 'Post Display'
+        self.left = 500
+        self.top = 250
+        self.width = 450
+        self.height = 400
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        listWidget = QListWidget(self)
+        for x in posts:
+            QListWidgetItem("User: " + x.idUser + "\t Title: " + x.title, listWidget)
+            QListWidgetItem("Description: " + x.description, listWidget)
+            QListWidgetItem("Link: " + x.url, listWidget)
+            QListWidgetItem("________________________________________________________________________________________",
+                listWidget)
+        listWidget.resize(450, 400)
+
+        window_layout = QVBoxLayout(self)
+        window_layout.addWidget(listWidget)
+        self.setLayout(window_layout)
+
+
 
 
 if __name__ == '__main__':
